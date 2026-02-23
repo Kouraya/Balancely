aa3ed9a34de081cb45cbdbfd4b94dc9bdfe96a00
Kouraya <Karpina.kathleen@gmail.com>
2026-02-23 13:01:05 +0100

    Delete pages/1_Dashboard.py

--- pages/1_Dashboard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Dashboard · Balancely", page_icon="⚖️", layout="wide")
-
-from shared import setup
-user_settings, t, currency_sym = setup()
-if user_settings is None:
-    st.stop()
-
-page_dashboard.render(st.session_state['user_name'], user_settings, t, currency_sym)
