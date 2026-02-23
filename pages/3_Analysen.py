bb14a2576fb85f9238be527d13600c60414b4c49
Kouraya <Karpina.kathleen@gmail.com>
2026-02-23 13:01:25 +0100

    Delete pages/3_Analysen.py

--- pages/3_Analysen.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from shared import setup
-import pages.analytics as page_analytics
-
-user_settings, t, currency_sym = setup("Analysen · Balancely")
-if user_settings is None:
-    st.stop()
-
-page_analytics.render(st.session_state['user_name'], currency_sym)
