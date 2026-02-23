db70934eac5025a9f4de0f44cf014eadc7ec09db
Kouraya <Karpina.kathleen@gmail.com>
2026-02-23 13:01:17 +0100

    Delete pages/2_Transaktionen.py

--- pages/2_Transaktionen.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from shared import setup
-import pages.transactions as page_transactions
-
-user_settings, t, currency_sym = setup("Transaktionen · Balancely")
-if user_settings is None:
-    st.stop()
-
-page_transactions.render(st.session_state['user_name'], currency_sym)
